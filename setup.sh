9c3ad9d0c6af7956f428297f06b6b1f18fb32732
Vladd Cantor <vladdcantor@gmail.com>
2021-12-10 16:36:06 +0000

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 sudo apt install scrcpy git curl neovim vim nodejs npm -y &&
-sudo apt remove cmdtest &&
-sudo apt remove yarn &&
+sudo apt remove cmdtest -y &&
+sudo apt remove yarn -y &&
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&
-sudo apt-get install yarn
+sudo apt-get install yarn -y &&
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
