111e03db69fb31d312a9acedd4d4ffd934441822
Vladd Cantor <vladdcantor@gmail.com>
2021-12-10 08:55:54 +0000

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-sudo apt scrcpy install git curl neovim vim -y && 
+#sudo apt install scrcpy git curl neovim vim -y && 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
-nvimdir="~/.config/nvim/"
+usrdir="/home/lubuntu/.config/nvim/"
 
-if [-d "$nvimdir"]; then
-	echo "nvim config folder exists!"
+if [ -d "$usrdir" ]
+then
+	echo "THE DIRECTORY ${usrdir} ALREADY EXISTS!"
 	exit 1
 else
-	echo "cloning config file." &&
+	echo "CLONING CONFIG FILE..." &&
 	touch ~/.config/nvim/init.vim &&
 	cd ~/.config &&
 	git clone https://github.com/VladdyHell/config-files.git &&
